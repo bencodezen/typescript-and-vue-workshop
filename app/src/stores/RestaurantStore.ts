aea8ed447055393abe6117e11dceb3d174c53170
Ben Hong <ben@bencodezen.io>
2022-08-30 15:33:50 -0500

    feature: add restaurant store

---
+++ app/src/stores/RestaurantStore.ts
@@ -0,0 +1,49 @@
+import type { Restaurant } from "@/types";
+import { defineStore } from "pinia";
+
+type StateShape = {
+  list: Restaurant[]
+}
+
+export const useRestaurantStore = defineStore('RestaurantStore', {
+  state: (): StateShape => ({
+    list: [
+      {
+        id: '9f995ce4-d2fc-4d00-af1d-6cb1647c6bd3',
+        name: 'Quiche From a Rose',
+        address: '283 Thisisnota St.',
+        website: 'www.quichefromarose.com',
+        status: 'Want to Try',
+      },
+      {
+        id: 'ae62a3da-791b-4f44-99a1-4be1b0ec30b8',
+        name: 'Tamago Never Dies',
+        address: '529 Letsgofora Dr.',
+        website: 'www.tamagoneverdies.com',
+        status: 'Recommended',
+      },
+      {
+        id: '9b361dae-2d44-4499-9940-97e188d41a32',
+        name: 'Penne For Your Thoughts',
+        address: '870 Thisisa St.',
+        website: 'www.penneforyourthoughts.com',
+        status: 'Do Not Recommend',
+      },
+    ]
+  }),
+  getters: {
+    numberOfRestaurants: (state): number => {
+      return state.list.length
+    }
+  },
+  actions: {
+    addRestaurant(payload: Restaurant) {
+      this.list.push(payload)
+    },
+    deleteRestaurant(payload: Restaurant) {
+      this.list = this.list.filter((restaurant: Restaurant) => {
+        return restaurant.id !== payload.name
+      })
+    }
+  },
+})
--- app/src/views/RestaurantsPage.vue
+++ app/src/views/RestaurantsPage.vue
@@ -5,59 +5,36 @@ import NewRestaurantForm from '../components/NewRestaurantForm.vue'
 import RestaurantCard from '../components/RestaurantCard.vue'
 import SideMenu from '../components/SideMenu.vue'
 import type { Restaurant } from '@/types'
+import { useRestaurantStore } from '@/stores/RestaurantStore'
 
-/**
- * Restaurant Module
- */
+const restaurantStore = useRestaurantStore()
 
-const restaurantList = ref<Restaurant[]>([
-  {
-    id: '9f995ce4-d2fc-4d00-af1d-6cb1647c6bd3',
-    name: 'Quiche From a Rose',
-    address: '283 Thisisnota St.',
-    website: 'www.quichefromarose.com',
-    status: 'Want to Try',
-  },
-  {
-    id: 'ae62a3da-791b-4f44-99a1-4be1b0ec30b8',
-    name: 'Tamago Never Dies',
-    address: '529 Letsgofora Dr.',
-    website: 'www.tamagoneverdies.com',
-    status: 'Recommended',
-  },
-  {
-    id: '9b361dae-2d44-4499-9940-97e188d41a32',
-    name: 'Penne For Your Thoughts',
-    address: '870 Thisisa St.',
-    website: 'www.penneforyourthoughts.com',
-    status: 'Do Not Recommend',
-  },
-])
+const restaurantList = restaurantStore.list
 
 const filteredRestaurantList = computed((): Restaurant[] => {
-  return restaurantList.value.filter((restaurant) => {
+  return restaurantList.filter((restaurant) => {
     if (restaurant.name) {
       return restaurant.name.toLowerCase().includes(filterText.value.toLowerCase())
     } else {
-      return restaurantList.value
+      return restaurantList
     }
   })
 })
 
 const addRestaurant = (payload: Restaurant) => {
-  restaurantList.value.push(payload)
+  restaurantStore.addRestaurant(payload)
   hideForm()
 }
 
-const deleteRestaurant = (payload: Restaurant) => {
-  restaurantList.value = restaurantList.value.filter((restaurant) => {
-    return restaurant.id !== payload.name
-  })
-}
+// const deleteRestaurant = (payload: Restaurant) => {
+//   restaurantList.value = restaurantList.value.filter((restaurant) => {
+//     return restaurant.id !== payload.name
+//   })
+// }
 
-const numberOfRestaurants = computed((): number => {
-  return filteredRestaurantList.value.length
-})
+// const numberOfRestaurants = computed((): number => {
+//   return filteredRestaurantList.value.length
+// })
 
 /**
  * New Form Module
